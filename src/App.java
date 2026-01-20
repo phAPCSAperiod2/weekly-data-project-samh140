e9f2dedce367812c760e79cc8cdec8c5e5f370f1
samh140 <residingblue@gmail.com>
2026-01-20 18:41:05 +0000

    Added insights for the user. Program is usable now.

--- src/App.java
+++ src/App.java
@@ -21,7 +21,7 @@ public class App {
         // TODO 2: Give information about your program
         //         Ask the user about their goals (if applicable)
         // -------------------------------------------------------------
-        System.out.println("Welcome");
+        System.out.println("Welcome to this program! We will help you analyze your sleep patterns over the week.");
 
         // -------------------------------------------------------------
         // TODO 3: Create an array to hold 7 days of data
@@ -40,7 +40,7 @@ public class App {
         //         - Re-prompt if the value is invalid
         // -------------------------------------------------------------
         for (int i = 0; i < weekData.length; i++) {
-            System.out.print("Enter sleep hours for day " + (i + 1) + ": ");
+            System.out.print("Please enter your sleep hours for this day " + (i + 1) + ": ");
             double value = scanner.nextDouble();
             while (value < 0) {
                 System.out.print("Invalid input. Please enter a non-negative value for day " + (i + 1) + ": ");
@@ -65,30 +65,66 @@ public class App {
         //
         //         Use clear labels and formatted output if needed
         // -------------------------------------------------------------
+        while (true) {
+            System.out.println("What data would you like to see?");
+            System.out.println("1. Total sleep hours\n2. Average sleep hours\n3. Minimum sleep hours\n4. Maximum sleep hours\n5. Show Data\n6. Quit\nEnter your choice (1-6):");
+            int choice = scanner.nextInt();
+            if (choice == 1) {
+                System.out.println("Total sleep hours: " + data.getTotal());
+            } else if (choice == 2) {
+                System.out.println("Average sleep hours: " + data.getAverage());
+                            if (data.getAverage() < 6) {
+                                System.out.println("You should try to get more sleep next week!");
+                            }
+                            else if (data.getAverage() <= 9) {
+                                System.out.println("You got the optimal amount of sleep this week! Keep it up!");
+                            }
+                            else {
+                                System.out.println("You slept a lot this week! Make sure you are using your time wisely!");
+                            }
+            } else if (choice == 3) {
+                System.out.println("Minimum sleep hours: " + data.getMin());
+                            if (data.getMin() < 4) {
+                                System.out.println("This is very unhealthy. Try managing your time better");
+                            }
+                            else if (data.getMin() <= 7) {
+                                System.out.println("Try getting more sleep on your low days.");
+                            }
+                            else {
+                                System.out.println("Nice job, you are maintaining a good sleep schedule!");
+                            }
 
-        System.out.println("What data would you like to see?");
-        System.out.println("1. Total sleep hours\n2. Average sleep hours\n3. Minimum sleep hours\n4. Maximum sleep hours\n5. Quit\nEnter your choice (1-5):");
-        int choice = scanner.nextInt();
-        if (choice == 1) {
-            System.out.printf("Total sleep hours: %.2f\n", data.getTotal());
-        } else if (choice == 2) {
-            System.out.printf("Average sleep hours: %.2f\n", data.getAverage());
-        } else if (choice == 3) {
-            System.out.printf("Minimum sleep hours: %.2f\n", data.getMin());
-        } else if (choice == 4) {
-            System.out.printf("Maximum sleep hours: %.2f\n", data.getMax());
-        } else if (choice == 5) {
-            System.out.println("Goodbye!");
-        } else {
-            System.out.println("Invalid choice.");
+            } else if (choice == 4) {
+                System.out.println("Maximum sleep hours: " + data.getMax());
+                            if (data.getMax() > 12){
+                                System.out.println("You are oversleeping! Please make sure you aren't procrasinating or being too lazy!");
+                            }
+                            else if (data.getMax() >= 7) {
+                                System.out.println("Great job on getting enough sleep!");
+                            }
+                            else {
+                                System.out.println("Try to get more sleep on your best days!");
+                            }
+            } else if (choice == 5) {
+                System.out.println("Here is your sleep data for the week:\nTry and find patterns in your sleep!");
+                System.out.println(data.toString());
+            } else if (choice == 6) {
+                System.out.println("Goodbye!");
+                break;
+            } else {
+                System.out.println("Invalid choice.");
+            }
+
         }
 
         // -------------------------------------------------------------
         // TODO 7: Display the full week of data
         //         Use the toString() method from WeeklyData
         // -------------------------------------------------------------
-        System.out.println("Here is your sleep data for the week:");
-        System.out.println(data.toString());
+        //System.out.println();
+        //System.out.println("Here is your sleep data for the week:");
+        //System.out.println(data.toString());
+        //System.out.println();
 
         // -------------------------------------------------------------
         // TODO 8: Give the user insights about their week
@@ -96,7 +132,9 @@ public class App {
         //         --> "You were very hydrated this week!"
         //         --> etc.
         // -------------------------------------------------------------
-
+        
+        
+        // insights were given after each method was called!
 
     }
 }
